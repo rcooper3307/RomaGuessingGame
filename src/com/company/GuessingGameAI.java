3954a320d8b3897e745e868aa547010297d9d13e
rcooper <rcooper3307@bths.edu>
2018-10-11 08:43:35 -0400

    cbvjhf

--- src/com/company/GuessingGameAI.java
+++ src/com/company/GuessingGameAI.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class GuessingGameAI {
     public static void main(String[] args) {
-        System.out.print("Wanna play a number guessing game? Neat!!! You pick a number.");
-        double computerGuess = Math.random() * 101;
+        System.out.print("Wanna play a number guessing game? Neat!!! You pick a number from 1-100.");
+        int min = 1;
+        int max = 100;
+        int computerGuess = max/2;
         Scanner input = new Scanner(System.in);
-        System.out.print("Shashasha! Now, is your secret number " + computerGuess + "? That's totally right, isn't it??");
-        if (!(input.nextDouble() = computerGuess) && (input.nextDouble() > computerGuess)) {
-            System.out.print("...Isn't it??? No??? I need to go lower then, huh? Easy! ");
-            computerGuess = computerGuess - 10;
-        } else if (input.nextDouble() < computerGuess) {
-            System.out.print("...Isn't it??? No??? I need to go higher then, huh? Easy!");
-            computerGuess = computerGuess + 10;
-        } else {
-            System.out.print("Shashasha! I knew I'd get it right. First try, too!");
+        int userNumber = input.nextInt();
+        System.out.print(" Now, is your secret number "+computerGuess+"? That's totally right, isn't it??");
+        if (computerGuess != userNumber)
+        {
+            if (input.equals("higher")){
+            {
+                System.out.print("No??? I need to go higher then, huh? Easy! ");
+                min = computerGuess;
+                computerGuess = (min + max) / 2;
+            }
+            if (input.equals("higher")){
+
+            }
         }
-    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int computerGuess = (int) (Math.random() * 10 + 1);
         System.out.print("I want you to guess my secret number! It's between one and ten!");
         Scanner input = new Scanner(System.in);
-        while((input.nextInt() != 7)) {
+        while((input.nextDouble() != computerGuess)) {
             System.out.print("Guess again, dummy! Remember, from 1-10");
         }
-        System.out.println("You got it! The secret number was 7");
-
+        System.out.println("You got it! The secret number was "+computerGuess+"!");
 	// write your code here
     }
 }
