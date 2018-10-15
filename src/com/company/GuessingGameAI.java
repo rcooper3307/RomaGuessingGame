7c9108423a29196e3b5562af3640af142ed4da04
rcooper3307 <rcooper3307@bths.edu>
2018-10-15 18:27:14 -0400

    bing bong! done

--- src/com/company/GuessingGameAI.java
+++ src/com/company/GuessingGameAI.java
@@ -5,36 +5,43 @@ import java.util.Scanner;
 public class GuessingGameAI {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello there, stranger! What's your name?");
+        String userName = input.nextLine();
         int min = 1;
         int max = 100;
-        System.out.println("Wanna play a number guessing game? Neat!!! You pick a number from 1-"+max+".");
+        int guessNumber = 0;
+        System.out.println("Okay, "+userName+"! Wanna play a number guessing game? Neat!!! You pick a number from 1-"+max+".");
         int computerGuess = max / 2;
-        Scanner input = new Scanner(System.in);
         int userNumber = input.nextInt();
-        System.out.println(" Now, is your secret number " + computerGuess + "? That's right, isn't it??");
+        System.out.println(" Now, "+userName+", is your secret number " + computerGuess + "? That's right, isn't it??");
         while (computerGuess != userNumber)
         {
             String highLowCorrect = input.nextLine();
+            guessNumber = guessNumber + 1;
             if (highLowCorrect.equals("higher"))
             {
                 System.out.println("No??? I need to go "+highLowCorrect+" then, huh? Easy! ");
                 min = computerGuess;
                 computerGuess = (min + max) / 2;
-                System.out.println("Is your number "+computerGuess+"?");
+                guessNumber = guessNumber + 1;
+
+                System.out.println("Is your number "+computerGuess+", "+userName+"?");
             }
             if (highLowCorrect.equals("lower"))
             {
                 System.out.println("No??? I need to go "+highLowCorrect+" then, huh? Easy!" );
                 max = computerGuess;
                 computerGuess = (min + max) / 2;
-                System.out.println("Is your number "+computerGuess+"?");
+                guessNumber = guessNumber + 1;
+                System.out.println("Is your number "+computerGuess+", "+userName+"?");
             }
         }
         System.out.println("I feel like that was close. Is that actually correct?");
         String correct = input.nextLine();
         if (correct.equals("correct"))
         {
-            System.out.println("Yes!!! I got it on my first try, too!");
+            System.out.println("Yes!!! It only took me "+guessNumber+" tries, too!");
         }
     }
 }
